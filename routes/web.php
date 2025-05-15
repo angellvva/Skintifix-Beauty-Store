8d439f029c34aa74bf6ca397be9427b3756d9467
dwindaaudia <151892320+dwindaaudia@users.noreply.github.com>
2025-05-15 21:45:27 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::get('/catalog', [HomeController::class, 'allProducts'])
 ->name('catalog');
 
+
 // route untuk login
 Route::get('/login', function (){
     return view('auth.login');
