1b7bbe2472b54307d10add99b78a044ac946c325
raynashera <rsherachang@student.ciputra.ac.id>
2025-05-15 16:03:35 +0700

    footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="text-center" style="background-color: #fbd3de;">
+{{-- <footer class="text-center" style="background-color: #fbd3de;">
   <div class="container pt-4">
     <section class="mb-4">
       <!-- WhatsApp -->
@@ -24,11 +24,79 @@
       ></a>
     </section>
   </div>
-  <!-- Grid container -->
+  <!-- Grid container --> --}}
 
-  <!-- Copyright -->
-  <div class="text-center text-white p-3" style="background-color: #e965a7;">
-    © 2025 Skintifix Beauty Store. All rights reserved. 
-  </div>
-  <!-- Copyright -->
+
+<!-- Footer Start -->
+<div class="container-fluid text-white-50 pt-5" style="background-color: #e965a7;">
+    <div class="container py-5">
+        <div class="pb-4 mb-4 border-bottom border-light">
+            <div class="row g-4">
+                <div class="col-lg-3">
+                    <a href="#">
+                        <h1 class="text-white mb-0">Skintifix</h1>
+                        <p class="text-white mb-0">Your beauty pal.</p>
+                    </a>
+                </div>
+                <div class="col-lg-9">
+                    <div class="d-flex justify-content-end pt-3">
+                        <a class="btn btn-outline-light me-2 btn-md-square rounded-circle" href="https://x.com/"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light me-2 btn-md-square rounded-circle" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light me-2 btn-md-square rounded-circle" href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-md-square rounded-circle" href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row g-5">
+            <div class="col-lg-5 col-md-6">
+                <div class="footer-item">
+                    <h4 class="text-white mb-3">Why People Like Us?</h4>
+                    <p class="mb-4 text-white">Our products are meticulously crafted with premium ingredients, ensuring the highest quality standards for your skincare and beauty regimen. Experience transformative beauty with our curated collection.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="d-flex flex-column text-start footer-item">
+                    <h4 class="text-white mb-3">Account</h4>
+                    <a class="btn-link text-white" href="{{ route('cart') }}">Shopping Cart</a>
+                    <a class="btn-link text-white" href="{{ route('wishlist') }}">Wishlist</a>
+                    <a class="btn-link text-white" href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log Out</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="footer-item">
+                    <h4 class="text-white mb-3">Contact</h4>
+                    <p class="text-white">Address: Universitas Ciputra, Surabaya, IDN</p>
+                    <p class="text-white">Email: skintifix@gmail.com</p>
+                    <p class="text-white">Phone: +62 812 3456 7890</p>
+                    <p class="text-white mb-1">Payment Accepted</p>
+                    <img src="{{ asset('images/payment.png') }}" class="img-fluid" alt="Payment Methods">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
+
+
+
+<!-- Copyright Start -->
+<div class="container-fluid bg-pink text-white py-3" style="background-color: #e965a7;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center text-md-start mb-2 mb-md-0">
+                <span class="text-white"><i class="fas fa-copyright me-2"></i>© 2025 Skintifix Beauty Store. All rights reserved. </span>
+            </div>
+            <div class="col-md-6 text-center text-md-end">
+                <a href="https://wa.me/+6281230300020" class="text-white me-2"><i class="fab fa-whatsapp"></i></a>
+                <a href="https://instagram.com" class="text-white"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Copyright End -->
+
+
 </footer>
