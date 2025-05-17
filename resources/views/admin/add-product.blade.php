f77bca24a2d1732d9cdfb3610654958a5d23849a
raynashera <rsherachang@student.ciputra.ac.id>
2025-05-17 23:05:46 +0700

    Update add-product.blade.php

--- resources/views/admin/add-product.blade.php
+++ resources/views/admin/add-product.blade.php
@@ -4,7 +4,7 @@
     <div class="container my-4">
         <div class="row mb-4">
             <div class="col-12">
-                <h2 class="fw-bold">Add Product</h2>
+                <h2 class="fw-bold" style="color: #e965a7;">Add Product</h2>
                 <p class="text-muted m-0">Capek deh besok lagi</p>
             </div>
         </div>
